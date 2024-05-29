02098d046b294dcdfe1030bfcd93dd7385642c51
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-05-29 12:39:39 +0700

    b4 add more package

--- tsx-evogym/src/App.tsx
+++ tsx-evogym/src/App.tsx
@@ -1,5 +1,11 @@
+import { Navbar } from './components/Navbar';
+
 function App() {
-  return <div className='text-7xl'>a</div>;
+  return (
+    <div className='app bg-gray-50'>
+      <Navbar />
+    </div>
+  );
 }
 
 export { App };
---
+++ tsx-evogym/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+useState;
+
+type Props = {};
+
+export function Navbar({}: Props) {
+  return <div>Navbar</div>;
+}
