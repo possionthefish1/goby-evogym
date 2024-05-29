6f6a5d0e46be95042a7a0c330b35d4815977aa06
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-05-29 11:42:18 +0700

    b4 optional

---
+++ tsx-evogym/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <div className="text-7xl">Ab</div>;
+}
+
+export default App;
---
+++ tsx-evogym/tailwind.config.js
@@ -0,0 +1,20 @@
+import fluid, { extract, fontSize } from "fluid-tailwind";
+import reset from "tw-reset";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	presets: [reset],
+	plugins: [fluid],
+	content: { files: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], extract },
+	theme: {
+		/** @type {import('fluid-tailwind').FluidThemeConfig} */
+		fluid: ({ theme }) => ({
+			defaultScreens: ["20rem", theme("screens.lg")],
+			// eslint-disable-next-line
+			defaultContainers: [, theme("containers.2xl")],
+		}),
+		fontSize,
+		fontFamily: {},
+		extend: {},
+	},
+};
